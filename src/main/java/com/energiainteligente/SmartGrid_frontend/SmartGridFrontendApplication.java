c57e621a41e3f8ebf9e97f815928a3f97f21e1d9
ylizzr <ylizarazor@ucentral.edu.co>
2025-05-06 14:05:51 -0500

    correccion estructura

---
+++ src/main/java/com/energiainteligente/SmartGrid_frontend/SmartGridFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.SmartGrid_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGridFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGridFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energiainteligente/SmartGrid_frontend/SmartGridFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.SmartGrid_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGridFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
